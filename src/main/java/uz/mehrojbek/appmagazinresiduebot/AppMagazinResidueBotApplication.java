975b43dc4dd6696bf0b57801d2022a040ddac107
Mehrojbek <mehrojbek970510@gmail.com>
2021-07-31 15:43:31 +0500

    update

--- src/main/java/uz/mehrojbek/appmagazinresiduebot/AppMagazinResidueBotApplication.java
+++ src/main/java/uz/mehrojbek/appmagazinresiduebot/AppMagazinResidueBotApplication.java
@@ -2,10 +2,24 @@ package uz.mehrojbek.appmagazinresiduebot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class AppMagazinResidueBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppMagazinResidueBotApplication.class, args);
+        while (true){
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "https://git.heroku.com/tvkdbot.git/";
+            restTemplate.exchange(url, HttpMethod.GET,null,String.class);
+            try {
+                TimeUnit.MINUTES.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/uz/mehrojbek/appmagazinresiduebot/controller/HelloController.java
+++ src/main/java/uz/mehrojbek/appmagazinresiduebot/controller/HelloController.java
@@ -1,8 +1,14 @@
 package uz.mehrojbek.appmagazinresiduebot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String hi(){
+        return "Salom";
+    }
 
 }
